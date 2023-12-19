e3a5ee45feff7883b6ab829aa262e17a3c5593f8
jairo51067 <jairocardenas05@gmail.com>
2023-12-18 21:12:19 -0400

    funcionalidad de los botones ver mas y ver menos

--- script_masmenos.js
+++ script_masmenos.js
@@ -1,19 +1,11 @@
-function mostar(){
-  document.getElementById('extra').style.display = "block"; 
-  location.reload(); 
-  document.getElementById('mostramasmenos').style.display = "none";
+function mostar() {
+  document.getElementById("extra").style.display = "block";
+  document.getElementById("mostrarmasmenos").style.display = "none";
+  
 }
 
-function ocultar(){
-  document.getElementById('extra').style.display = "none";
- 
+function ocultar() {
+  document.getElementById("extra").style.display = "none";
+  document.getElementById("mostrarmasmenos").style.display = "block";
 }
-
-
-
-
-
-
-
-
 
